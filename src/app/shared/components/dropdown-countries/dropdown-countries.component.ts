d8b2b9b1e42368f051c78b5a3b6f78c478d7f8ee
Mauro Molina <molinamau37@gmail.com>
2024-12-27 20:59:25 -0500

    building auth module

---
+++ src/app/shared/components/dropdown-countries/dropdown-countries.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-countries',
+  templateUrl: './dropdown-countries.component.html',
+  styleUrl: './dropdown-countries.component.scss'
+})
+export class DropdownCountriesComponent {
+
+}
